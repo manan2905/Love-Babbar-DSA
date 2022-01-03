00af7462eb3b7d65b5409b279f3a58172a749b15
Manan Anand <57489875+manan2905@users.noreply.github.com>
2022-01-03 19:24:58 +0530

    Update 1. Reverse Array.py

--- Arrays/1. Reverse Array.py
+++ Arrays/1. Reverse Array.py
@@ -2,9 +2,9 @@ def rev(arr):
   n = len(arr)
   # [1,2,3,4,5]
   for i in range(n//2):
-    arr[i],arr[n-i] = arr[n-i],arr[i]
+    arr[i],arr[n-i-1] = arr[n-i-1],arr[i]
     
     
 arr = [int(i) for i in input().split()]
 rev(arr)
-print(*ans)    
+print(*arr)    
